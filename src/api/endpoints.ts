69770e57a2bc4852d1d10482bd2ee593611b667b
abdulwahidjaved <ansariabdulwahidjaved@gmail.com>
2026-06-29 06:22:42 +0530

    done

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
+  import.meta.env.VITE_API_BASE_URL || "https://backend-r1zu.onrender.com/api";
 
 export const ENDPOINTS = {
   allNews: `${API_BASE_URL}/news/`,
